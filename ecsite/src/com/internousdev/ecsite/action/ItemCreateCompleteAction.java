aa4b45d64160a33a246220478bd5726128cf6946
furukawahiroto <lirukq@gmail.com>
2019-08-27 15:22:11 +0900

    0827

---
+++ ecsite/src/com/internousdev/ecsite/action/ItemCreateCompleteAction.java
@@ -0,0 +1,11 @@
+package com.internousdev.ecsite.action;
+
+public class ItemCreateCompleteAction extends ActionSupport implements SessionAware{
+	public Map<String,Object> session;
+	public String execute()throws SQLException{
+		ItemCreateComp;
+	}
+	
+	
+
+}
